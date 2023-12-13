344ca562f2d8e208d2ec8bb36b0baf091860dca3
Sean Gyuris <sean.gyuris@mcgregorcoxall.com>
2023-12-13 12:06:00 +1100

    added zip file uploads and reads

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,12 @@ from flask import flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import zipfile
 from flask import render_template
+import geopandas as gpd
+import io
+from fiona.io import ZipMemoryFile
+import fiona
+import tempfile
+from fiona.io import ZipMemoryFile
 
 
 def create_app(test_config=None):
@@ -15,6 +21,9 @@ def create_app(test_config=None):
     app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
     ALLOWED_EXTENSIONS = {'zip'}
     
+    folder = 'hi'
+    TEMP = tempfile.gettempdir()
+    app.config['TEMP_FOLDER'] = TEMP
 
 
     if test_config is None:
@@ -37,13 +46,40 @@ def create_app(test_config=None):
         if request.method == 'POST':
             # retrieve the file sent via post request (input name is data_zip_file)
             file = request.files['data_zip_file']
-            file_like_object = file.stream._file  
-            zipfile_ob = zipfile.ZipFile(file_like_object)
-            file_names = zipfile_ob.namelist()
-            file_names = [file_name for file_name in file_names if file_name.endswith(".shp")]
-            files = [(zipfile_ob.open(name).read(),name) for name in file_names]
-            return str(files)
-            print(file_names)
+            # files = request.files.to_dict('data_zip_file')
+            # for file in files:     #image will be the key 
+            #     file_name = files[file].filename
+            #     files[file].save(os.path.join(app.config['TEMP_FOLDER'], file))
+            # files = request.files.getlist(multi=False, 'data_zip_file')
+            # for file in files:
+            # files.save(os.path.join(app.config['TEMP_FOLDER'], files.filename)) 
+            # for file in uploaded_files:
+            #     file.save(os.path.join(app.config['TEMP_FOLDER'], file.filename))
+            # file_like_object = file.stream._file  
+            # zipfile_ob = zipfile.ZipFile(file_like_object)
+            # bytes_buffer = io.BytesIO()
+            # content = file.read()
+            
+            # file_names = zipfile_ob.namelist()
+            # for file_name in file_names:
+            #     if file_name.endswith(".shp"):
+            #         shapefile = file_name
+            # with ZipMemoryFile(file_like_object) as zip:
+            #     gdf = gpd.read_file(zip)
+            
+            path = (os.path.join(app.config['TEMP_FOLDER'], file.filename))
+            file.save(path)
+            gdf = gpd.read_file(str(path))
+            # path = os.path.join(app.config['TEMP_FOLDER'], file)
+            # gdf = gpd.read_file(rf'{path}')
+                    # print(collection.schema)
+            # item = zipfile_ob.open(shapefile)
+            # files = [(zipfile_ob.open(name).read() for name in file_names)]
+            # shape = zipfile_ob.open(shapefile).read()
+            
+            # if "polygon" in gdf.geom_type:
+            return str(gdf.geom_type[0])
+                    
         if request.method == 'GET':
             flav = os.path.join(app.config['UPLOAD_FOLDER'], 'MCGC_AGREEMENT_LOGO-01.jpg')
             return render_template('home.html', logo = full_filename, flavicon = flav)
