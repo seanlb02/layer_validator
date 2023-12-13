88e2d8310e666568945c3db3938ae6cc3c89c705
Sean Gyuris <sean.gyuris@mcgregorcoxall.com>
2023-12-13 14:22:55 +1100

    finishing output table

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -47,13 +47,44 @@ def create_app(test_config=None):
             # retrieve the file sent via post request (input name is data_zip_file)
             file = request.files['data_zip_file']
             
+
             
             path = (os.path.join(app.config['TEMP_FOLDER'], file.filename))
             file.save(path)
-            # Create the geodataframe
+            # Create the geodataframe and set up a spatial index:
             gdf = gpd.read_file(str(path))
+            gdf.sindex
+            # geom err check:
+            if False in gdf.is_valid.values:
+                    geom_error = "Yes"
+            else: 
+                    geom_error = "No"
+            # corrupt geometry check:
+            corr_file = ""
+            if True in gdf.is_empty.values:
+                 corr_file = "Yes"
+            else:
+                 corr_file= "No"
+            # empty attribute table:
+            if gdf.shape[1] >= 4:
+                 no_attr = "Present"
+            else:
+                 no_attr = "None"
+            # Topology check
+            # We only check Topology/Geometry errors for polygons and multilines
+            if 'Point' in gdf.geom_type:
+                pass
+            else:
+                # new dummy dataframe to host any overlapping layers
+                sdf = gdf.sindex.query(gdf.geometry, predicate='overlaps')
+                # if there are any:
+                if sdf.size != 0:
+                     topo_error = "Yes"
+                else:
+                     topo_error = "No"
             flav = os.path.join(app.config['UPLOAD_FOLDER'], 'MCGC_AGREEMENT_LOGO-01.jpg')
-            return render_template('result.html', layer_name = file.filename[:-4], geometry = str(gdf.geom_type[0]), projection = gdf.crs.name, corrupt = "", geo_err = "", overlap = "",  logo = full_filename, flavicon = flav)
+            os.remove(path)
+            return render_template('result.html', layer_name = file.filename[:-4], geometry = str(gdf.geom_type[0]), projection = gdf.crs.name, corrupt = corr_file, attributes = no_attr, geo_err = geom_error, overlap = topo_error,  logo = full_filename, flavicon = flav)
                     
         if request.method == 'GET':
             flav = os.path.join(app.config['UPLOAD_FOLDER'], 'MCGC_AGREEMENT_LOGO-01.jpg')
