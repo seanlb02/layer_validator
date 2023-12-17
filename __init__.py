7ec9c8b455675fe51330a19acce4bba01638d89b
seanlb02 <seangyuris1@gmail.com>
2023-12-17 17:31:11 +1100

    adding multi shape zip file iterator

--- __init__.py
+++ __init__.py
@@ -62,7 +62,8 @@ def create_app(test_config=None):
                 warning = ""
                 # retrieve the file sent via post request (the 'input' element name is data_zip_file)
                 file = request.files['data_zip_file']
-                
+                # result dicts get stored in this list to be iterated in the html table via jinja
+                res_list = []
 
 
                 # Sanitize first input (.zip)
@@ -80,40 +81,55 @@ def create_app(test_config=None):
                                 raise fiona.errors.DriverError
                         else:
                             with ZipMemoryFile(data) as zip:
-                                with zip.open(f'{file_names[0][:-4]}.shp') as collection:
-                                    gdf = gpd.GeoDataFrame.from_features([feature for feature in collection], crs=collection.crs)
-                            gdf.sindex
-                            # geom err check:
-                            if False in gdf.is_valid.values:
-                                geom_error = "Yes"
-                            else: 
-                                geom_error = "No"
-                            # corrupt geometry check:
-                            corr_file = ""
-                            if True in gdf.is_empty.values:
-                                corr_file = "Yes"
-                            else:
-                                corr_file= "No"
-                            # empty attribute table:
-                            if gdf.shape[1] >= 4:
-                                no_attr = "Present"
-                            else:
-                                no_attr = "None"
-                            # Topology check
-                            # We only check Topology/Geometry errors for polygons and multilines
-                            if 'Point' in gdf.geom_type:
-                                pass
-                            else:
-                                # new dummy dataframe to host any overlapping layers
-                                sdf = gdf.sindex.query(gdf.geometry, predicate='overlaps')
-                                # if there are any:
-                                if sdf.size != 0:
-                                    topo_error = "Yes"
-                                else:
-                                    topo_error = "No"
+                                for shapefile in file_names:
+                                     if shapefile.rsplit('.', 1)[1].lower() == 'shp':
+                                        res_item = {}
+                                        layer_name = shapefile[:-4]
+                                        geometry = ""
+                                        projection = ""
+                                        corr_file = ""
+                                        no_attr = ""
+                                        geo_err = ""
+                                        topo_err = ""
+                                         # with zip.open(f'{file_names[0][:-4]}.shp') as collection:
+                                        with zip.open(f'{shapefile}') as collection:
+                                            gdf = gpd.GeoDataFrame.from_features([feature for feature in collection], crs=collection.crs)
+                                        gdf.sindex
+                                        geometry = str(gdf.geom_type[0])
+                                        projection = gdf.crs.name
+                                        # geom err check:
+                                        if False in gdf.is_valid.values:
+                                            geom_error = "Yes"
+                                        else: 
+                                            geom_error = "No"
+                                        
+                                        # corrupt geometry check:
+                                        corr_file = ""
+                                        if True in gdf.is_empty.values:
+                                            corr_file = "Yes"
+                                        else:
+                                            corr_file= "No"
+                                        # empty attribute table:
+                                        if gdf.shape[1] >= 4:
+                                            no_attr = "Present"
+                                        else:
+                                            no_attr = "None"
+                                        # Topology check
+                                        # We only check Topology/Geometry errors for polygons and multilines
+                                        if 'Point' in gdf.geom_type:
+                                            pass
+                                        else:
+                                            # new dummy dataframe to host any overlapping layers
+                                            sdf = gdf.sindex.query(gdf.geometry, predicate='overlaps')
+                                            # if there are any:
+                                            if sdf.size != 0:
+                                                topo_error = "Yes"
+                                            else:
+                                                topo_error = "No"
+                                        res_list = [*res_list, [layer_name, geometry, projection, corr_file, no_attr, geom_error, topo_error]]
                             flav = os.path.join(app.config['UPLOAD_FOLDER'], 'MCGC_AGREEMENT_LOGO-01.jpg')
-                            return render_template('result.html', layer_name = file.filename[:-4], Warning = warning, geometry = str(gdf.geom_type[0]), projection = gdf.crs.name, corrupt = corr_file, attributes = no_attr, geo_err = geom_error, overlap = topo_error,  logo = full_filename, list = file_names, flavicon = flav)
-
+                            # return render_template('result.html', layer_name = file.filename[:-4], Warning = warning, geometry = str(gdf.geom_type[0]), projection = gdf.crs.name, corrupt = corr_file, attributes = no_attr, geo_err = geom_error, overlap = topo_error,  logo = full_filename, list = file_names, flavicon = flav)
+                            return render_template('result.html', result = res_list, logo = full_filename, flavicon = flav)
                 
             except fiona.errors.DriverError: 
                 pass
